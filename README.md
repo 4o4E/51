cfd7dad6b7753e5a51d594cd224f5e96fca5311a
4o4E <869951226@qq.com>
2022-11-28 23:50:58 +0800

    update

--- README.md
+++ README.md
@@ -1365,3 +1365,46 @@ int main() {
 ```
 
 </details>
+
+### 定时器作业
+
+> 采用定时器T0工作方式1控制LED亮65ms灭65ms，循环闪烁
+
+<details>
+  <summary>点击查看代码</summary>
+
+```c
+#include "reg51.h"
+
+typedef unsigned char u8;
+typedef unsigned int u16;
+
+sbit LED0 = P2 ^ 0;
+
+void init0() {
+  TMOD |= 0X01;            //选择为定时器0模式 工作方式1
+  TH0 = 0XFC, TL0 = 0X18;  //给定时器赋初值 定时1ms
+  ET0 = 1;                 //打开定时器0中断允许
+  EA = 1;                  //打开总中断
+  TR0 = 1;                 //打开定时器
+}
+
+void exti0() interrupt 1 {
+  static u16 i;            //定义静态变量i
+  TH0 = 0XFC, TL0 = 0X18;  //给定时器赋初值，定时1ms
+  i++;
+  if (i == 65) {
+    i = 0;
+    LED0 = !LED0;
+  }
+}
+
+int main() {
+  init0();
+  while (1) {
+  }
+  return 0;
+}
+```
+
+</details>
--- src/main.c
+++ src/main.c
@@ -3,104 +3,28 @@
 typedef unsigned char u8;
 typedef unsigned int u16;
 
-sbit KEY3 = P3 ^ 2;
-sbit KEY4 = P3 ^ 3;
 sbit LED0 = P2 ^ 0;
-sbit LED1 = P2 ^ 1;
-
-sbit LSA = P2 ^ 2;
-sbit LSB = P2 ^ 3;
-sbit LSC = P2 ^ 4;
-
-void delay(u16 us) {
-  while (us--) {
-  }
-}
 
 void init0() {
-  IT0 = 1;
-  EX0 = 1;
-  EA = 1;
+  TMOD |= 0X01;            //选择为定时器0模式 工作方式1
+  TH0 = 0XFC, TL0 = 0X18;  //给定时器赋初值 定时1ms
+  ET0 = 1;                 //打开定时器0中断允许
+  EA = 1;                  //打开总中断
+  TR0 = 1;                 //打开定时器
 }
 
-void init1() {
-  IT1 = 1;
-  EX1 = 1;
-  EA = 1;
-}
-
-static int i, j, k;
-
-int arr1492[] = {0x06, 0x66, 0x6f, 0x5b};
-
-// 数码管动态扫描方法显示 1 4 9 2（循环5次）
-void exti0() interrupt 0 {
-  if (KEY3 != 0)
-    return;
-  for (j = 0; j < 5; j++) {
-    for (k = 0; k < 500; k++) {
-      for (i = 0; i < 4; i++) {
-        // 设置数码管位数
-        switch (i) {
-          case 0:
-            LSC = 1, LSB = 1, LSA = 1;
-            break;
-          case 1:
-            LSC = 1, LSB = 1, LSA = 0;
-            break;
-          case 2:
-            LSC = 1, LSB = 0, LSA = 1;
-            break;
-          case 3:
-            LSC = 1, LSB = 0, LSA = 0;
-            break;
-        }
-        // 设置显示
-        P0 = arr1492[i];
-        delay(10);
-      }
-    }
-    for (i = 0; i < 4; i++) {
-      // 设置数码管位数
-      switch (i) {
-        case 0:
-          LSC = 1, LSB = 1, LSA = 1;
-          break;
-        case 1:
-          LSC = 1, LSB = 1, LSA = 0;
-          break;
-        case 2:
-          LSC = 1, LSB = 0, LSA = 1;
-          break;
-        case 3:
-          LSC = 1, LSB = 0, LSA = 0;
-          break;
-      }
-      // 设置显示
-      P0 = 0;
-      delay(10);
-    }
-    delay(500000);
+void exti0() interrupt 1 {
+  static u16 i;            //定义静态变量i
+  TH0 = 0XFC, TL0 = 0X18;  //给定时器赋初值，定时1ms
+  i++;
+  if (i == 65) {
+    i = 0;
+    LED0 = !LED0;
   }
-}
-
-// 驱动8只LED灯奇数灯与偶数灯交叉闪烁（循环5次）
-void exti1() interrupt 2 {
-  delay(1000);
-  if (KEY4 != 0)
-    return;
-  for (i = 0; i < 5; i++) {
-    P2 = 85;
-    delay(20000);
-    P2 = ~P2;
-    delay(20000);
-  }
-  P2 = 0xff;
 }
 
 int main() {
   init0();
-  init1();
   while (1) {
   }
   return 0;
